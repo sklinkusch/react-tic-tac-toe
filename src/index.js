759bb6740c45e8a91f784540dda3febd5cf2a07b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-01 15:22:22 +0200

    added method jumpTo to Game

--- src/index.js
+++ src/index.js
@@ -78,6 +78,12 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext
     });
   }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
