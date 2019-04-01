60c7a7f9fa1793424616a9b5e42bc0e00db78d2a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-01 15:24:52 +0200

    modified handleClick

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ class Game extends React.Component {
     };
   }
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -75,6 +75,7 @@ class Game extends React.Component {
           squares: squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
